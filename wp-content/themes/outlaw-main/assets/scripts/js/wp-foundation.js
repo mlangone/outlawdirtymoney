04ee09adc335079d9219be90eddc65f22f0a5d9e
Jarod Landaker <jarodlandaker@gmail.com>
2019-09-25 16:08:50 -0700

    [hopefully] fixed cascading JS fail when original nav not present

--- wp-content/themes/outlaw-main/assets/scripts/js/wp-foundation.js
+++ wp-content/themes/outlaw-main/assets/scripts/js/wp-foundation.js
@@ -3,31 +3,34 @@ These functions make sure WordPress
 and Foundation play nice together.
 */
 jQuery(document).ready(function() {
+	var	$ =	jQuery;
+	
 	
 	// Remove empty P tags created by WP inside of Accordion and Orbit
-	jQuery('.accordion p:empty, .orbit p:empty').remove();
+	$('.accordion p:empty, .orbit p:empty').remove();
 
 	// Adds Flex Video to YouTube and Vimeo Embeds
-	jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
-		if ( jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5 ) {
-		  jQuery(this).wrap("<div class='widescreen responsive-embed'/>");
+	$('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function(i, n) {
+		var	$this =	$(n);
+		
+		if (($this.innerWidth() / $this.innerHeight()) > 1.5) {
+			$this.wrap('<div class="widescreen responsive-embed"/>');
 		} else {
-		  jQuery(this).wrap("<div class='responsive-embed'/>");
+			$this.wrap('<div class="responsive-embed"/>');
 		}
 	});
 
-	var menuButton = document.getElementById("menuButton");
-    menuButton.addEventListener("click", function(e) {
-		menuButton.classList.add("is-active");
+	var	$menuButton =	$('#menuButton');
+
+	$menuButton.on('click', function(e) {
 		e.preventDefault();
-	  });
+		
+		$menuButton.addClass('is-active');
+	});
+	// Removes the x on the burger menu when off canvas is closed
+	$('#off-canvas').on('closed.zf.offcanvas', function() {
+		$menuButton.removeClass('is-active');
+		// getInspectionTalk.removeClass('close-out');
+	});
 
-  // Removes the x on the burger menu when off canvas is closed
-  jQuery("#off-canvas").on("closed.zf.offcanvas", function() {
-    menuButton.classList.remove("is-active");
-    // getInspectionTalk.classList.remove('close-out');
-  });
-
-
-
-}); 
+});
